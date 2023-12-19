21543b8117595b2b7bd0ddcab8d6ea30b865173d
maurosanchezcastineiras <maurosanchezcastineiras@gmail.com>
2023-12-19 01:41:07 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/sumadosnumeros/SumaDosNumeros.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sumadosnumeros;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aleja
+ */
+public class SumaDosNumeros {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Calculadora de suma de dos números");
+        
+        System.out.print("Ingresa el primer número: ");
+        
+        int num1 = scanner.nextInt();
+
+        
+        System.out.print("Ingresa el segundo número: ");
+        
+        int num2 = scanner.nextInt();
+
+        int suma = num1 + num2;
+
+        System.out.println("La suma de " + num1 + " y " + num2 + " es igual a: " + suma);
+
+        scanner.close();
+    }
+}
